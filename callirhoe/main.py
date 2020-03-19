37aa37cc7f0889884ac0d5769d4dfe35a49d328a
Claudiu Diaconeasa <diaconeasaclaudiu@gmail.com>
2020-03-19 19:46:16 +0000

    Final modification

--- callirhoe/main.py
+++ callirhoe/main.py
@@ -1,14 +1,18 @@
 import os
-from flask import Flask, request, send_file
+import multiprocessing
+from flask import Flask, request, send_file, Response
 from uuid import uuid4
 from werkzeug.utils import secure_filename
 import creator
+from flask_cors import CORS
 
 UPLOAD_FOLDER = '/home/claudiu/calendarGenerator/callirhoe/uploads'
 
 application = Flask(__name__)
 application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CORS(application)
 
+process_mapper = {}
 
 @application.route('/generate', methods=['POST'])
 def generate_pdf():
@@ -17,8 +21,36 @@ def generate_pdf():
     filename = secure_filename(idName + '.csv')
     file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
 
-    creator.main_program(os.path.join(application.config['UPLOAD_FOLDER'], filename), idName)
-    return send_file(secure_filename(idName + '.pdf'))
+    #creator.main_program(os.path.join(application.config['UPLOAD_FOLDER'], filename), idName)
+    p = multiprocessing.Process(target=creator.main_program, args=(os.path.join(application.config['UPLOAD_FOLDER'], filename), idName))
+    p.start()
+
+    process_mapper[idName] = p
+
+    #return send_file(secure_filename(idName + '.pdf'))
+    return idName
+
+
+@application.route('/poll')
+def poll_pdf():
+    idName = request.args['idname']
+    filename = secure_filename(idName + '.pdf')
+    
+    if idName not in process_mapper.keys():
+        return 'Id not found'
+
+    if process_mapper[idName].is_alive():
+        calendar_ready = False
+    else:
+        if os.path.exists(os.path.join(filename)):
+            calendar_ready = True
+        else:
+            calendar_ready = False
+
+    if calendar_ready:
+        return send_file(filename)
+    else:
+        return 'Calendar not generated yet'
 
 @application.route('/')
 def home():
--- callirhoe/wsgi.py
+++ callirhoe/wsgi.py
@@ -1,13 +1,5 @@
 from main import application
 import os
-import logging
-
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    application.logger.handlers = gunicorn_logger.handlers
-    application.logger.setLevel(gunicorn_logger.level)
-
-application.logger.info(os.getcwd())
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=1337)
